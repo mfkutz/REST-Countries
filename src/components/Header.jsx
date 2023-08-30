82e16dd3200845b89a86c8e0bd5d128d08685c9a
Martin Kutzner <mfkutz@gmail.com>
2023-08-30 10:32:13 -0300

    styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,38 @@
-import { RiMoonLine } from "react-icons/ri";
+import { useEffect, useState } from "react";
+import { RiMoonLine, RiMoonFill } from "react-icons/ri";
 
 const Header = () => {
+
+  const [theme, setTheme] = useState("light")
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+  }, [theme])
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
+
   return (
-    <div className=" bg-white shadow-md flex   ">
+    <div className=" bg-white shadow-md flex dark:bg-DarkBlue transition duration-300 ">
       <div className="wrapper flex justify-between items-center py-[18px] px-[10px] w-full">
-        <div className="text-[25px] font-bold">
+        <div className="text-[25px] font-bold dark:text-white">
           Where in the world?
         </div>
-        <div className="text-[16px] font-semibold flex gap-3 items-center cursor-pointer">
-          <RiMoonLine /> <span>Dark Mode</span>
+        <div className="text-[16px] font-semibold flex gap-3 items-center cursor-pointer dark:text-white" onClick={handleThemeSwitch}>
+          {theme !== "dark" ? (
+            <div className="flex items-center gap-2">
+              <RiMoonFill size={"20px"} />
+              <span>Dark Mode</span></div>
+          ) : (
+            <div className="flex items-center gap-2">
+              <RiMoonLine size={"20px"} color="white" />
+              <span>Light Mode</span>
+            </div>)}
         </div>
       </div>
     </div>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { BiSearchAlt2, BiChevronDown } from "react-icons/bi";
 import Card from "./Card";
 
-
 const Section = () => {
 
   const [data, setData] = useState([])
@@ -25,12 +24,11 @@ const Section = () => {
     fetchData()
   }, [])
 
-
   function getShuffledData(data) {
-    return [...data].sort(() => Math.random() - 0.5);
+    /* return [...data].sort(() => Math.random() - 0.5); */
+    return [...data].sort((a, b) => a.name.localeCompare(b.name));
   }
 
-  // Filtrar los datos según el término de búsqueda
   const filteredData = shuffledData.filter(item =>
     item.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
     (selectedRegion === "" || item.region === selectedRegion)
@@ -40,16 +38,13 @@ const Section = () => {
     setSelectedRegion(region);
   }
 
-
-
-
   return (
-    <div className="pt-[42px] h-full z-0 flex flex-col w-full ">
+    <div className="pt-[42px] h-full z-0 flex flex-col w-full pb-12 ">
       <div className="wrapper flex justify-between pb-8 px-[10px] flex-wrap w-full gap-4">
         <div className=" relative">
           <input
             type="text"
-            className="shadow-md border text-[14px] pl-16 h-[55px] w-[480px] rounded-md custom"
+            className="shadow-md border text-[14px] pl-16 h-[55px] w-[480px] rounded-md custom dark:bg-DarkBlue dark:text-white "
             placeholder="Search for a country..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
@@ -59,12 +54,12 @@ const Section = () => {
           </div>
         </div>
 
-        <button className="shadow-md bg-white text-[14px] font-semibold rounded-md w-[200px] text-left pl-6 h-[55px] relative" onClick={() => setRegion(!region)}>
+        <button className="shadow-md bg-white text-[14px] font-semibold rounded-md w-[200px] text-left pl-6 h-[55px] relative dark:bg-DarkBlue dark:text-white" onClick={() => setRegion(!region)}>
           {selectedRegion === "" ? 'All regions' : selectedRegion}
           <div className="absolute right-2 top-4">
             <BiChevronDown size='23px' />
           </div>
-          <ul className={`bg-white absolute top-16 w-[200px] left-0 p-4 rounded-md shadow-md flex flex-col gap-2 cursor-default ${region ? 'block' : 'hidden'}`}>
+          <ul className={`bg-white absolute top-16 w-[200px] left-0 p-4 rounded-md shadow-md flex flex-col gap-2 cursor-default dark:bg-DarkBlue ${region ? 'block' : 'hidden'}`}>
             <li className="p-1 cursor-pointer hover:font-bold" onClick={() => handleRegion("")}>All Regions</li>
             <li className="p-1 cursor-pointer hover:font-bold" onClick={() => handleRegion("Africa")}>Africa</li>
             <li className="p-1 cursor-pointer hover:font-bold" onClick={() => handleRegion("Americas")}>America</li>
